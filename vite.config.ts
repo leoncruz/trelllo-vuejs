a4d430d4bfaf775ae10419938febc8885dbbe133
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-04-01 22:57:29 -0300

    config: configure path for models folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import path from 'path';
+
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       components: path.resolve(__dirname, './src/components'),
       pages: path.resolve(__dirname, './src/pages'),
-      assets: path.resolve(__dirname, './src/assets')
+      assets: path.resolve(__dirname, './src/assets'),
+      models: path.resolve(__dirname, './src/models')
     }
   }
 });
